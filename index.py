c558bd80da5bf4972a0cc7f1ff71f7f4ca89c281
YevhenFomichov <yevhen@sonohaler.com>
2024-07-31 19:42:18 +0200

    index.py

--- index.py
+++ index.py
@@ -2,23 +2,12 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from projects import combined_mdi, mdi, dpi, vx, ek, ek_old
 
-def plot_old_new(old_data, new_data, title):
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
-    
-    ax1.plot(old_data['x'], old_data['y'])
-    ax1.set_title('Old Model ' + title)
-    
-    ax2.plot(new_data['x'], new_data['y'])
-    ax2.set_title('New Model ' + title)
-    
-    st.pyplot(fig)
-
 def main():
     plt.rcParams["figure.figsize"] = (10, 4)
 
     st.sidebar.title("Menu")
     project = st.sidebar.radio('Which project would you like to test:', 
-                               ['Combined MDI', 'MDI', 'DPI', 'VX', 'EK'], 
+                               ['Combined MDI', 'MDI', 'DPI', 'VX', 'EK', 'EK_old'], 
                                key='project_selector')
 
     if project == 'Combined MDI':
@@ -30,9 +19,9 @@ def main():
     elif project == 'VX':
         vx.run()
     elif project == 'EK':
-        old_data = ek_old.run()
-        new_data = ek.run()
-        plot_old_new(old_data, new_data, 'EK')
+        ek.run()
+    elif project == 'EK_old':
+        ek_old.run()
 
 if __name__ == "__main__":
     main()
